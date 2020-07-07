660dd347fdedb6e0b1305f2d8ac3feb0d4443d5d
nagase123 <nagase0926@gmail.com>
2020-07-08 02:30:40 +0900

    scss追加、デザイン修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import NavTabs from '../components/Tab'
 import styles from '../components/layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import '../styles/styles.scss'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <div className={utilStyles.backgroundColor}>
     <header className={styles.header}>
       <NavTabs />
     </header>
     <Component {...pageProps} />
-  </>
+  </div>
 )
 
 export default App
