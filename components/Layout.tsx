6bf12df7e5d774d66e6ff9f6ddbd79173d2f5d98
nagase123 <nagase0926@gmail.com>
2020-07-09 22:56:01 +0900

    サーチコンソール設定

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,6 +7,10 @@ export const siteTitle = 'のすけのポートフォリオサイト'
 
 export default function Layout({ children }: any) {
   const trackingId: string = process.env.ga_tracking_id!
+  const description: string =
+    'Webエンジニアとして活動している、Nagese Ryunosukeのポートフォリオサイトです。'
+  const url: string = 'https://nosuke-portfolio.web.app/'
+  const image = '/images/profile.jpg'
   return (
     <div className={styles.container}>
       <Head>
@@ -19,18 +23,27 @@ export default function Layout({ children }: any) {
             __html: `(function(){ window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${trackingId}');})`,
           }}
         />
-        <meta
-          name="description"
-          content="Webエンジニアとして活動している、Nagese Ryunosukeのポートフォリオサイトです。"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
+        <title>のすけのポートフォリオ</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="blog" />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={image} />
+        <meta property="og:site_name" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nosuke926" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={siteTitle} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+        {/* <link rel="canonical" href={url} /> */}
+        {/* <link rel="apple-touch-icon" href={image} /> */}
+        <meta
+          name="google-site-verification"
+          content="7LNV_CLMRiHwoVKdUbRlCdyJ0TIhYsWJh2PT3cJEO9w"
+        />
       </Head>
 
       <main>{children}</main>
