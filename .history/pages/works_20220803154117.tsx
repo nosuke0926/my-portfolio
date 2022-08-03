96ed5625bc5bcb772ffcacde7d886fae01def7a0
nagase ryunosuke <nagase0926@gmail.com>
2022-08-03 16:14:36 +0900

    WorksにPDF変換アプリを追加

--- .history/pages/works_20220803154117.tsx
+++ .history/pages/works_20220803154117.tsx
@@ -14,7 +14,7 @@ const WorksView = (props: any) => (
             alt="アプリ（さぶすく）"
             description="サブスクリプションを賢く管理するアプリ"
             appTitle="さぶすく"
-            useTechnology={['Flutter', 'Firebase']}
+            useTechnology={['Flutter', 'Firebase', 'RevenueCat']}
             appDescription="サービスの無料期間終了前にプッシュ通知でお知らせします。毎月・毎年のサブスクリプションサービスにいくらお金を使っているのかもグラフで可視化します。"
             iosUrl="https://apps.apple.com/jp/app/id1506451600?mt=8"
             productHuntUrl="https://www.producthunt.com/posts/subsc?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-subsc"
@@ -29,7 +29,7 @@ const WorksView = (props: any) => (
             alt="アプリ（爆速Todoメモ）"
             description="起動して2秒でメモできるアプリ"
             appTitle="爆速Todoメモ"
-            useTechnology={['Flutter']}
+            useTechnology={['Flutter', 'SQLite']}
             appDescription="アプリを起動すると同時にキーボードが開きます。また記入と閲覧が同時にできるよう一画面を分割した新しいメモアプリです。"
             iosUrl="https://apps.apple.com/jp/app/id1511398926?mt=8"
             androidUrl="https://play.google.com/store/apps/details?id=com.nosuke.rapid&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
@@ -42,7 +42,7 @@ const WorksView = (props: any) => (
             alt="アプリ（Yomoo）"
             description="ニューモフィズムな読書管理アプリ"
             appTitle="Yomoo"
-            useTechnology={['Flutter']}
+            useTechnology={['Flutter', 'SQLite', 'RevenueCat']}
             appDescription="2020年~のUIデザインのトレンドであるNeumorphismを取り入れた読書管理アプリです。楽天ブックスAPI、openBDと連携してバーコード読み取りからの書籍登録が可能です。"
             iosUrl="https://apps.apple.com/jp/app/id1516860157?mt=8"
             androidUrl="https://play.google.com/store/apps/details?id=com.nosuke.yomoo&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
@@ -55,7 +55,7 @@ const WorksView = (props: any) => (
             alt="アプリ（推しPay）"
             description="推しへの愛を管理&シェアするアプリ"
             appTitle="推しPay"
-            useTechnology={['Flutter', 'Firebase']}
+            useTechnology={['Flutter', 'Firebase', 'RevenueCat']}
             appDescription="「推し」にいくらお金を使っているのかを記録&SNSにシェアできるアプリです。"
             iosUrl="https://apps.apple.com/jp/app/id1523768983?mt=8"
             androidUrl="https://play.google.com/store/apps/details?id=com.nosuke.oshi_pay&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
@@ -68,8 +68,8 @@ const WorksView = (props: any) => (
             alt="アプリ（TubeNote）"
             description="YouTubeを見ながらメモできるアプリ"
             appTitle="TubeNote"
-            useTechnology={['Flutter', 'Firebase', 'Algolia']}
-            appDescription="YouTubeを見ながらリアルタイムにメモできるアプリです。"
+            useTechnology={['Flutter', 'Firebase', 'RevenueCat', 'Algolia']}
+            appDescription="YouTubeを見ながらリアルタイムにメモできるアプリです。メモは動画のURLとともにSNSにシェアできます。現在日本語・英語・韓国語に対応しています。"
             iosUrl="https://apps.apple.com/jp/app/id1535634801?mt=8"
             productHuntUrl="https://www.producthunt.com/posts/tubenote?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-tubenote"
             productHuntPostId="279810"
@@ -83,7 +83,7 @@ const WorksView = (props: any) => (
             alt="アプリ（ミグル）"
             description="簡単に頭痛を記録するアプリ"
             appTitle="ミグル"
-            useTechnology={['Flutter', 'Firebase']}
+            useTechnology={['Flutter', 'Firebase', 'RevenueCat']}
             appDescription="頭痛の記録・振り返りに特化したシンプルなアプリです。"
             iosUrl="https://apps.apple.com/jp/app/id1564686045?mt=8"
             androidUrl="https://play.google.com/store/apps/details?id=com.nosuke.migraine&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
@@ -109,7 +109,7 @@ const WorksView = (props: any) => (
             alt="アカウントフォルダ"
             description="サービスのアカウントを一括管理するアプリ"
             appTitle="アカウントフォルダ"
-            useTechnology={['Flutter']}
+            useTechnology={['Flutter', 'Hive']}
             appDescription="サービスのアカウント・パスワードを一括管理するアプリです。データは暗号化・端末に保存されるためセキュリティも安心です。"
             iosUrl="https://apps.apple.com/jp/app/id1606877526?mt=8"
             androidUrl="https://play.google.com/store/apps/details?id=com.nosuke.account_holder&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
@@ -144,7 +144,7 @@ const WorksView = (props: any) => (
         </p>
         <p>
           <TransitionsModal
-            src="/images/products/pdf_converter.png"
+            src="/images/products/diary.png"
             alt="PDF変換 - 写真や画像をPDFにすぐに変換"
             description="画像をPDFに変換するアプリ"
             appTitle="PDF変換 - 写真や画像をPDFにすぐに変換"
